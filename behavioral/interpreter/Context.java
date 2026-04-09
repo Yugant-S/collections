de053dd0325463c3e34dc075138356cebb31c245
Yugant-S <yugantsonwani@gmail.com>
2026-04-09 15:30:49 +0530

    Add Context class for interpreter pattern

---
+++ behavioral/interpreter/Context.java
@@ -0,0 +1,12 @@
+class Context {
+
+    private String input;
+
+    public Context(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+}
